5e31e677cddbbbf20bd97107cb55e3eb8bc00c18
anthony <anthony.c.da.cruz@gmail.com>
2021-10-07 13:09:15 -0400

    added all engine functions

--- 1007/Main.cpp
+++ 1007/Main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 using namespace std;
 
-int main()
+// Init function. Sets up SDL and all of its subsystems.
+
+void Init()
 {
+	cout << "Initializing game..." << endl;
+}
+// run function. Contains the primary loop.
+void HandleEvents()
+{
+	cout << "Handleing..." << endl;
+}
+void Update()
+{
+	cout << "Processing..." << endl;
+}
+void Render()
+{
+	cout << "Rendering..." << endl;
+}
+int Run()
+{
+	bool isRunning = true;
+	char input;
+
+	Init();
+	while (isRunning) // game loop.
+	{
+		HandleEvents(); //Input.
+		Update(); //Processing.
+		Render(); //Output.
+		cout << "Do you want to quit? [y/n]: ";
+		cin >> inupt;
+		if ( input == 'y')
+	}
 
 }
+// main function. enter point of every C++ program.
+int main()
+{
+	return Run();
+}
+
